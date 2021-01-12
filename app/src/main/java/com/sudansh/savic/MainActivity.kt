93281fbeb302d3c35cae117b92c57170769f5217
Sudhanshu Singh <sudanshs@gmail.com>
2021-01-12 18:34:51 +0400

    screens

--- app/src/main/java/com/sudansh/savic/MainActivity.kt
+++ app/src/main/java/com/sudansh/savic/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.sudansh.savic
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,6 +42,23 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navView.setNavigationItemSelectedListener {
+            if (it.title == getString(R.string.share)) {
+                openShare()
+                return@setNavigationItemSelectedListener true
+            }
+            return@setNavigationItemSelectedListener false
+        }
+    }
+
+    private fun openShare() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Share your result with your friends")
+            type = "text/plain"
+        }
+        startActivity(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -53,4 +71,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+
 }
--- app/src/main/java/com/sudansh/savic/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/sudansh/savic/ui/settings/SettingsViewModel.kt
@@ -1,10 +1,10 @@
-package com.sudansh.savic.ui.gallery
+package com.sudansh.savic.ui.settings
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
